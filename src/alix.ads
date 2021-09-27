fa85cbfc1011e946bdac091d8f29701ac1644ab5
Fraser Wilson <fraser@thewhitelion.eu>
2021-09-27 14:16:30 +0200

    Version 0.1.1

--- src/alix.ads
+++ src/alix.ads
@@ -4,7 +4,7 @@ package Alix is
 
    Version_Major   : constant := 0;
    Version_Minor   : constant := 1;
-   Version_Release : constant := 0;
-   Version_Name    : constant String := "0.1.0";
+   Version_Release : constant := 1;
+   Version_Name    : constant String := "0.1.1";
 
 end Alix;
